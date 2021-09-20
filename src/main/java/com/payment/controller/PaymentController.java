416e7ef4c29f17d2e4dfe9c791728b0965f54398
Julio <julio@ruedaferrero.com>
2021-09-20 16:49:54 +0200

    Prueba test controller

--- src/main/java/com/payment/controller/PaymentController.java
+++ src/main/java/com/payment/controller/PaymentController.java
@@ -1,8 +1,10 @@
 package com.payment.controller;
+import com.payment.services.PaymentService;
 import com.payment.utils.Utils;
 import com.payment.model.Account;
 import com.payment.response.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -16,13 +18,21 @@ import javax.validation.Valid;
 @Slf4j
 @RestController
 public class PaymentController {
+    @Autowired
+    PaymentService service;
+
     @GetMapping("/")
     public ResponseEntity<Response> validatePayment(@Valid @RequestBody Account account, BindingResult result){
         log.info("Validating Account: {}", account);
-        if(result.hasErrors()){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, Utils.formatBindingResult(result));
-        }
         Response response = new Response();
+        if(result.hasErrors()){
+            response.setStatus("Bad request");
+        }
+        else {
+            response = service.validateAccount(account);
+            log.error("Holi22");
+
+        }
         return ResponseEntity.ok(response);
     }
 }
--- src/test/java/com/payment/test/controller/ValidateAccountShould.java
+++ src/test/java/com/payment/test/controller/ValidateAccountShould.java
@@ -1,10 +1,40 @@
 package com.payment.test.controller;
 
+import com.payment.controller.PaymentController;
+import com.payment.model.Account;
+import com.payment.response.Response;
+import com.payment.services.PaymentService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@RunWith(SpringRunner.class)
 public class ValidateAccountShould {
+    ResponseEntity<Response> response;
+    @Autowired
+    private PaymentController paymentController;
 
+    @Mock
+    BindingResult a;
+
+    @Test
+    public void Return() {
+        Account account = new Account("", "656", "holiii");
+        a.addError(new ObjectError("Soy un objeto dummy", "Fallo porque quiero"));
+        response = paymentController.validatePayment(account, a);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+      //  assertEquals(response.getBody().getStatus(), "Bad request");
+    }
 }
