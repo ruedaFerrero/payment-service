30f00ee72c9e590070dc6ee0eab3c94ba9dd7a24
Julio <julio@ruedaferrero.com>
2021-09-20 16:14:53 +0200

    Test commit

--- src/test/java/com/payment/test/services/ValidateAccountShould.java
+++ src/test/java/com/payment/test/services/ValidateAccountShould.java
@@ -2,7 +2,10 @@ package com.payment.test.services;
 
 import static org.junit.Assert.assertEquals;
 
+import com.payment.services.PaymentServiceImp;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -10,24 +13,20 @@ import org.springframework.test.context.ActiveProfiles;
 import com.payment.model.Account;
 import com.payment.response.Response;
 import com.payment.services.PaymentService;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@RunWith(SpringRunner.class)
 public class ValidateAccountShould {
-
-	@Autowired
 	Response response;
 	@Autowired
-	PaymentService service;
+	private PaymentService service;
 	
 	@Test
 	public void ReturnMensageWhenCvvEqualsTo007() {
-		
 		Account account = new Account("4916585815127134", "007", "holiii");
-		
 		response = service.validateAccount(account);
-		
 		assertEquals(response.getStatus(),"cvv not valid");
 	}
-	
 }
