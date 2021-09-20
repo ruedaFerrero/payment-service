e1fa2d4e7d9f287de221090b650dccd1831052c9
Eva Terradillos Estévez <eterradillosextevez@gmail.com>
2021-09-20 16:53:49 +0200

    Test controller

--- src/test/java/com/payment/test/controller/ValidateAccountShould.java
+++ src/test/java/com/payment/test/controller/ValidateAccountShould.java
@@ -1,10 +1,24 @@
 package com.payment.test.controller;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.payment.model.Account;
+import com.payment.response.Response;
+import com.payment.services.PaymentService;
 
 @SpringBootTest
 @ActiveProfiles("test")
 public class ValidateAccountShould {
+	
+
+
 
 }
+
