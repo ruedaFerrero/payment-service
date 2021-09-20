8ffedf6385cd6ec1a46fae3af16d92d7c5023988
Julio <julio@ruedaferrero.com>
2021-09-20 12:48:48 +0200

    Updated controller

--- src/main/java/com/payment/controller/paymentController.java
+++ src/main/java/com/payment/controller/paymentController.java
@@ -1,5 +1,32 @@
 package com.payment.controller;
 
-public class paymentController {
+import com.payment.model.Account;
+import com.payment.services.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
+
+@Slf4j
+@RestController
+@RequestMapping("/")
+class paymentController {
+    @Autowired
+    private PaymentService serviceLayer;
+
+    public ResponseEntity<Response> validatePayment(@Valid @RequestBody Account account, BindingResult result){
+        log.info("Validating Account: {}", account);
+        if(result.hasErrors()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "");
+        }
+        Response response = serviceLayer.validate(account);
+        return ResponseEntity.ok(response);
+    }
 }
